ed840e0ee32dced8a08fdfd4b4067a1bc23ce110
IPandral <crocodilegaming747@gmail.com>
2023-09-04 00:20:32 +0800

    updated links
    i updated the battlenet link to grab the newest and i have also added in the function to remove sleep mode and to setup remote desktop

--- autoinstall.bat
+++ autoinstall.bat
@@ -28,10 +28,10 @@ echo Installing Epic Games Launcher...
 powershell.exe -Command "Invoke-WebRequest 'https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi' -OutFile 'C:\Temp\EpicInstaller.msi'"
 start /wait msiexec /i "C:\Temp\EpicInstaller.msi" /quiet
 
-:: Install Battle.net
+:: Download and install Battle.net
 echo Installing Battle.net...
-powershell.exe -Command "Invoke-WebRequest 'http://dist.blizzard.com/downloads/bna-installers/322d5bb9ae0318de3d4cde7641c96425/retail.1/Battle.net-Setup.exe' -OutFile 'C:\Temp\BattleNet-Setup.exe'"
-start /wait "" "C:\Temp\BattleNet-Setup.exe"
+powershell.exe -Command "Invoke-WebRequest 'https://www.battle.net/download/getInstallerForGame?os=win&gameProgram=BATTLENET_APP&version=Live' -OutFile 'C:\\Temp\\BattleNet-Setup.exe'"
+start /wait "" "C:\\Temp\\BattleNet-Setup.exe"
 
 :: Install Spotify
 echo Installing Spotify...
@@ -43,3 +43,17 @@ echo Cleaning up...
 rd /s /q C:\Temp
 
 echo All Done. All software is installed and network share is mounted.
+
+:: Disable sleep
+echo Disabling sleep...
+powercfg /change -standby-timeout-ac 0
+powercfg /change -hibernate-timeout-ac 0
+
+:: Enable Remote Desktop
+echo Enabling Remote Desktop...
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+:: Force Windows Update
+echo Forcing Windows Update...
+wuauclt /detectnow
+wuauclt /updatenow
